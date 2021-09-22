76575909acd4d03e113c3adc4348f993f90b238b
shiga.anna <shiga.anna@pocke.co.jp>
2021-09-22 17:52:02 +0900

    デザインを軽く作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,54 +1,22 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
+    <nav class="header has-shadow is-primary" role="navigation" aria-label="main navigation">
+      <a class="navbar-item" href="/">
+        <img src="~assets/K2.png" height="28">
+      </a>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <NuxtLink
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </aside>
+    <ul class="menu-list">
+      <li v-for="(item, key) of items" :key="key">
+        <NuxtLink :to="item.to">
+          <b-icon :icon="item.icon" /> {{ item.title }}
+        </NuxtLink>
+      </li>
+    </ul>
 
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
-    </section>
+    <div class="container column is-10">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
@@ -58,8 +26,8 @@ export default {
     return {
       items: [
         {
-          title: 'Home',
-          icon: 'home',
+          // title: 'Home',
+          // icon: 'home',
           to: { name: 'index' }
         }
       ]
